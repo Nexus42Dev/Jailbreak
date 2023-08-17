d980a44c10dd7acfef15a780577bdcdaa4e2a9e8
Nexus42 <66770433+Nexus42Dev@users.noreply.github.com>
2023-08-17 15:16:16 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ echo -e "Installing MacSploit"
 unzip ./MacSploit.zip
 
 echo -e "Patching Roblox"
-./insert_dylib /Applications/Roblox.app/Contents/MacOS/macsploit.dylib /Applications/Roblox.app/Contents/MacOS/RobloxPlayer --strip-codesig --all-yes
-mv ./macsploit.dylib /Applications/Roblox.app/Contents/MacOS/macsploit.dylib
+./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
 rm ./insert_dylib
 
 echo -e "Installing MacSploit App"
 mv ./MacSploit.app /Applications/MacSploit.app
 
-echo -e "Install Complete!"
+echo -e "Install Complete! Developed by Nexus42!"
